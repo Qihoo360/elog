3fe7d714b0cc39aea33c40e9ee0720c1ffe54adc
baotiao <baotiao@gmail.com>
2014-10-16 13:52:57 +0800

    change the git repo place

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -17,10 +17,11 @@ case "$1" in
     clean)
         rm -rf ../priv/*
         rm -rf *.o
-        if [ -d mario ]; then
-            (cd mario && make clean)
-            rm -rf mario
-        fi
+        rm -rf mario
+        # if [ -d mario ]; then
+        #     (cd mario && make clean)
+        #     (cd .. && rm -rf mario)
+        # fi
 
         ;;
 
@@ -37,7 +38,7 @@ case "$1" in
 
     get-deps)
         if [ ! -d mario ]; then
-            git clone git@github.com:baotiao/Mario.git ./mario
+            git clone git://github.com/baotiao/Mario.git ./mario
             (cd mario && git checkout $MARIO_VSN)
         fi
         ;;
@@ -49,7 +50,7 @@ case "$1" in
         export LD_LIBRARY_PATH="$BASEDIR/mario/lib:$LD_LIBRARY_PATH"
 
         if [ ! -d mario ]; then
-            git clone https://github.com/baotiao/Mario.git ./mario
+            git clone git://github.com/baotiao/Mario.git ./mario
             (cd mario && git checkout $MARIO_VSN)
         fi
         (cd mario && make)
